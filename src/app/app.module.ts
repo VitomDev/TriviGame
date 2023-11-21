62698945fc80f21058578926cc96393f86a29008
28victor <victorescos.b@gmail.com>
2023-11-21 14:02:00 +0100

    TriviGame.v0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './services/login/login.component';
+import { RegisterComponent } from './authentication/register/register/register.component';
+import { HomeComponent } from './home/home/home.component';
+import { PerfilComponent } from './perfil/perfil/perfil.component';
+import { JuegoComponent } from './juego/juego/juego.component';
+import { FinalJuegoComponent } from './juego/final-juego/final-juego.component';
+import { AddPreguntaComponent } from './preguntas/add-pregunta/add-pregunta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    PerfilComponent,
+    JuegoComponent,
+    FinalJuegoComponent,
+    AddPreguntaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/juego/final-juego/final-juego.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-final-juego',
+  templateUrl: './final-juego.component.html',
+  styleUrls: ['./final-juego.component.css']
+})
+export class FinalJuegoComponent {
+
+}
