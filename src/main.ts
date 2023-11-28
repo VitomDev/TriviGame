700e87d6225e79cdc6f6dcafafc2aed111002a2e
Víctor Escós <37983645+VitomDev@users.noreply.github.com>
2023-11-28 14:13:11 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,16 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const body = document.getElementById('bodyMain');
+let compFondo: boolean = true;
+
+if (body != null) {
+  if (compFondo) {
+    body.classList.remove('bodyJuego');
+    body.classList.add('bodyMenu');
+  } else {
+    body.classList.remove('bodyMenu');
+    body.classList.add('bodyJuego');
+  }
+}
