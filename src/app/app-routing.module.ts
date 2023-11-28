e8625b98f88425b26284326004a542f3394763f1
28victor <victorescos.b@gmail.com>
2023-11-28 13:44:24 +0100

    Modificación main files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuegoComponent } from './juego/juego/juego.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'juego', component: JuegoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   let message: string = 'Hello, world!';
+//   console.log(message);
+// });
+
+const body = document.getElementById('bodyMain');
+let compFondo: boolean = true;
+
+if (body != null) {
+  if (compFondo) {
+    body.classList.remove('bodyJuego');
+    body.classList.add('bodyMenu');
+  } else {
+    body.classList.remove('bodyMenu');
+    body.classList.add('bodyJuego');
+  }
+}
