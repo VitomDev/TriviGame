2d0aa8e76dd0951b6ea794485f8b5cdf3d84d324
= <=>
2023-12-12 14:20:41 +0100

    Ens peta el constructor de home.component.ts.
    Ens mirarem el home.service.ts a veure si pot tenir algun fallo.
    
    El html està casi acabat només falta que el back-end no peti i agafar la informació corresponent per el html.
    
    S'ha afegit l'environment i s'ha actualitzat les llibreries del node_module.ts.

--- src/app/home/services/home.service.ts
+++ src/app/home/services/home.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { usuarios } from '../modelDades/usuarios';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  
+  vUsuarios: usuarios[] = [];
+  bdUsuari = '/Usuaris/'
+  
+  constructor(private bd: AngularFireDatabase) { }
 
-  constructor() { }
+  getUsuari(){
+    return this.bd.list(this.bdUsuari).valueChanges();
+  }
+
+
+  getUsuari2(username: string){
+    // console.log(username)
+    return this.bd.object(this.bdUsuari + username).valueChanges();
+  }
+
+  pushPuntsUsuari(username: string, punts: number) {
+    const dades = {
+      username: username,
+      punts: punts,
+    }
+
+    this.bd.object(this.bdUsuari + dades.username).update(dades)
+      .then(d => {console.log("Dades guardades correctament")}) 
+      .catch(e => {console.log("Error al guardar dades")})
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyABcNrtRUCkp7F94sW9VKF2vhH0zgjY2BE',
+    authDomain: 'trivigame-a2461.firebaseapp.com',
+    databaseURL: 'https://trivigame-a2461-default-rtdb.firebaseio.com',
+    projectId: 'trivigame-a2461',
+    storageBucket: 'trivigame-a2461.appspot.com',
+    messagingSenderId: '75448870363',
+    appId: '1:75448870363:web:5c920712b497f26091f3fe',
+    measurementId: 'G-KJ3R14G36Z',
+  },
+};
